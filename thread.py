58aa4045d648270531210fb1da781e40b6b55139
jhassan12 <62314918+jhassan12@users.noreply.github.com>
2020-06-24 01:34:33 -0400

    Update thread.py

--- thread.py
+++ thread.py
@@ -1,9 +1,31 @@
 class Thread:
 	def __init__(self):
 		self.is_threading = False
+		self.is_finding_path = False
+		self.finished_state = False
+		self.path_exists = True
 
 	def turn_on(self):
 		self.is_threading = True
+		self.is_finding_path = True
 
 	def turn_off(self):
 		self.is_threading = False
+		self.is_finding_path = False
+		self.finished_state = True
+
+	def stop_search(self):
+		self.is_finding_path = False
+
+	def path_not_found(self):
+		self.path_exists = False
+
+	def reset_path_exists(self):
+		self.path_exists = True
+
+	def reset_finished_state(self):
+		self.finished_state = False
+
+
+
+
